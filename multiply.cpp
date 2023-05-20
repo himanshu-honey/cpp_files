b8ac4dd6ad7c9de180161c1ee8f1556ce2ee5758
Himanshu Maholia <himanshumaholia613@gmail.com>
2023-05-20 16:20:09 +0530

    ./

---
+++ multiply.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+  // stores two floating point numbers in num1 and num2 respectively
+  int num1=5;
+  int num2=8;
+ 
+  // performs multiplication and stores the result in product variable
+  int product = num1 * num2;  
+
+  cout << "Product = " << product;    
+    
+  return 0;
+}
